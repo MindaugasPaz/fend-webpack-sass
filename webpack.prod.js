a7bf15e331fa313f90a501f54071f75ed2c5c620
Mindaugas <m.pazereckas@gmail.com>
2020-01-14 06:29:42 +0100

    css problem

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,10 +2,15 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserPlgin = require('terser-webpack-plugin')
 
 module.exports = {
     entry: './src/client/index.js',
     mode: 'production',
+    optimization: {
+        minimizer: [new TerserPlgin({}), new OptimizeCSSAssetsPlugin({})]
+    },
     module: {
         rules: [
             {
